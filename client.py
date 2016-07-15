7e7ddf4281599ef37d6158a1f0e3d646585da7e9
Satya Bodduluri <skbodduluri@gmail.com>
2016-07-15 00:06:15 -0700

    first commit

---
+++ client.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python
+import pyaudio
+import wave
+import socket
+import signal
+import sys
+import time
+
+from docopt import docopt
+
+
+doc = """Moz
+
+Usage:
+  client.py <ip> <port> share <wavfile>
+  client.py <ip> <port> listen <room>
+  client.py (-h | --help)
+  client.py (-V | --version)
+
+Options:
+  -h --help     Show this screen.
+  -V --version  Show version.
+"""
+
+TCP_IP = "127.0.0.1"
+TCP_PORT = 5005
+
+# length of data to send
+BUFFER = 1024
+
+sock = None
+p = None
+stream = None
+
+
+def share(file, ip, port):
+    global sock, p, stream
+    # open the file for reading.
+    wf = wave.open(file, 'rb')
+
+    # create an audio object
+    p = pyaudio.PyAudio()
+
+    sock = socket.socket(socket.AF_INET,  # Internet
+                         socket.SOCK_STREAM)  # TCP
+    sock.connect((ip, port))
+    sock.send('0')  # identifier for sharer
+
+    print 'Room id: %s' % sock.recv(BUFFER)
+
+    # read data (based on the chunk size)
+    data = wf.readframes(BUFFER)
+
+    while data != '':
+        sock.send(data)
+        data = wf.readframes(BUFFER)
+
+    cleanup()
+
+
+def listen(room, ip, port):
+    global BUFFER, sock, p, stream
+    BUFFER = BUFFER * 2 * 2
+    # create an audio object
+    p = pyaudio.PyAudio()
+
+    # open stream based on the wave object which has been input.
+    stream = p.open(format=p.get_format_from_width(2),
+                    channels=2,
+                    rate=44100,
+                    output=True)
+
+    sock = socket.socket(socket.AF_INET,  # Internet
+                         socket.SOCK_STREAM)  # TCP
+    sock.connect((ip, port))
+
+    sock.send('1')  # identifier for listener
+    sock.send(room)
+
+    while True:
+        data = sock.recv(BUFFER)
+        if not data:
+            break
+        stream.write(data)
+
+    cleanup()
+
+
+def signal_handler(signal, frame):
+    cleanup()
+    sys.exit(0)
+
+
+def cleanup():
+    global sock, stream, p
+    if sock:
+        sock.close()
+    if stream:
+        stream.close()
+    if p:
+        p.terminate()
+
+
+def main():
+    signal.signal(signal.SIGINT, signal_handler)
+
+    args = docopt(doc, version='Moz 1.0')
+    try:
+        if args.get('share'):
+            share(args.get('<wavfile>'), args.get('<ip>'), int(args.get('<port>')))
+        elif args.get('listen'):
+            listen(args.get('<room>'), args.get('<ip>'), int(args.get('<port>')))
+    finally:
+        cleanup()
+
+
+if __name__ == '__main__':
+    main()
---
+++ server.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+import errno
+import pyaudio
+import signal
+import socket
+import sys
+import time
+import uuid
+
+from Queue import Empty, Queue
+from threading import Thread, Lock
+
+TCP_IP = "127.0.0.1"
+TCP_PORT = 5005
+
+sock = socket.socket(socket.AF_INET,  # Internet
+                     socket.SOCK_STREAM)  # TCP
+sock.bind((TCP_IP, TCP_PORT))
+sock.listen(5)
+
+BUFFER = 1024 * 2 * 2
+
+audio = {}  # {room: Queue<data>}
+listeners = {}  # {room: Set<socket>}
+lock = Lock()
+
+
+def sharer_handler(conn):
+    room = str(uuid.uuid4())
+    conn.sendall(room)
+    while True:
+        data = conn.recv(BUFFER)
+        if not data:
+            break
+        with lock:
+            if room not in audio:
+                audio[room] = Queue()
+                listeners[room] = set()
+        audio[room].put(data)
+
+
+def listener_handler(conn):
+    room = conn.recv(1024)
+    with lock:
+        if room not in audio:
+            audio[room] = Queue()
+            listeners[room] = set()
+    listeners[room].add(conn)
+    while True:
+        time.sleep(1)
+
+
+def spray():
+    while True:
+        with lock:
+            for room in audio:
+                if listeners[room]:
+                    try:
+                        data = audio[room].get_nowait()
+                    except Empty:
+                        continue
+                    for conn in listeners[room]:
+                        try:
+                            conn.sendall(data)
+                        except IOError, e:
+                            if e.errno != errno.EPIPE:
+                                raise e
+
+
+def client_thread(conn):
+    data = conn.recv(1)
+
+    try:
+        if data == '0':
+            sharer_handler(conn)
+        else:
+            listener_handler(conn)
+    finally:
+        with lock:
+            print 'here'
+            for members in listeners.values():
+                members.discard(conn)
+            conn.close()
+
+
+def cleanup():
+    global sock, stream, p
+    if sock:
+        sock.close()
+
+
+def signal_handler(signal, frame):
+    cleanup()
+    sys.exit(0)
+
+Thread(target=spray).start()
+
+# threads = []
+while True:
+    signal.signal(signal.SIGINT, signal_handler)
+
+    conn, addr = sock.accept()
+    print 'Connection address:', addr
+    thread = Thread(target=client_thread, args=(conn,))
+    thread.start()
+    # threads.append(thread)
